b6b8cd67a9f10546649c0a60bba6004a579cd2cd
Bars Margetsch <45538919+Xerren09@users.noreply.github.com>
2021-02-21 21:22:17 +0200

    commit for ex6

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var carRouter = require('./routes/car');
+var ownerRouter = require('./routes/owner');
+var carOwnerRouter = require('./routes/carowner');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/car', carRouter);
+app.use('/owner', ownerRouter);
+app.use('/carowner', carOwnerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/carowner.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const carowner = require('../models/carowner_model.js');
+
+router.get('/:id?',
+ function(request, response) {
+  if (request.params.id) {
+    carowner.getById(request.params.id, function(err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult);
+      }
+    });
+  } else {
+    carowner.get(function(err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult);
+      }
+    });
+  }
+});
+
+
+router.post('/', 
+function(request, response) {
+  carowner.add(request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(request.body);
+    }
+  });
+});
+
+
+router.delete('/:id', 
+function(request, response) {
+  carowner.delete(request.params.id, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+
+router.put('/:id', 
+function(request, response) {
+  carowner.update(request.params.id, request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+module.exports = router;
